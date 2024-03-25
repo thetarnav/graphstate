24b85068ebd48a331d7f67c057b248e7e405b7ee
Damian Tarnawski <gthetarnav@gmail.com>
2024-03-25 21:21:20 +0100

    Add test workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,31 @@
-odin build . -out:graphstate \
-	-o:aggressive \
-	-microarch:native \
+FLAGS="-out:graphstate \
 	-no-thread-local \
-	-no-bounds-check \
-	-disable-assert \
-	-obfuscate-source-code-locations \
 	-vet-unused \
 	-vet-shadowing \
 	-vet-style \
-	-vet-semicolon
+	-vet-semicolon"
+
+case $1 in
+	"release")
+		odin build . $FLAGS \
+			-o:aggressive \
+			-microarch:native \
+			-no-bounds-check \
+			-disable-assert \
+			-obfuscate-source-code-locations
+		;;
+	"debug")
+		odin build . $FLAGS -debug
+		;;
+	"client")
+		if [ ! -f "./graphstate" ]; then
+			echo "graphstate binary does not exist."
+			exit 1
+		fi
+
+		cat ./schema_test.graphql | ./graphstate > ./build/client.js
+		;;
+	*)
+		odin build . $FLAGS
+		;;
+esac
---
+++ test.js
@@ -0,0 +1,7 @@
+import * as t      from "node:test"
+import * as assert from "node:assert/strict"
+import * as client from "./build/client.js" // Generated
+
+t.test("test", () => {
+	
+})
